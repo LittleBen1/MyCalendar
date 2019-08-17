bad551e7761903e86f980a8e5b8835731384a4d1
Benjamin <brgaultier@gmail.com>
2019-08-17 18:28:47 +0200

    Added tutorial project

---
+++ ionic-youtube-tutorial/src/app/recipes/recipes.page.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
+import { RecipesService } from './recipes.service';
+
+@Component({
+  selector: 'app-recipes',
+  templateUrl: './recipes.page.html',
+  styleUrls: ['./recipes.page.scss'],
+})
+export class RecipesPage implements OnInit {
+
+  recipes: Recipe[] = [
+    {
+      id: 'r1',
+      title: 'Schnitzel',
+      imageUrl: 'https://www.thespruceeats.com/thmb/dFLHcwFFtDguBZGylUlDN8KaNaw=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/wiener-schnitzel-recipe-1447089-Hero-5b587d6c46e0fb0071b0059d.jpg',
+      ingredients: ['French Fries', 'Pork Meat', 'Salad']
+    },
+    {
+      id: 'r2',
+      title: 'Spaghetti',
+      imageUrl: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/homemade-spaghetti-sauce-horizontal-1530890913.jpg',
+      ingredients: ['Spaghetti', 'Tomatoes', 'Parmesan']
+    }
+  ];
+  constructor(private recipesService: RecipesService) { }
+
+  ngOnInit() {
+    this.recipes = this.recipesService.getAllRecipes();
+  }
+
+}
