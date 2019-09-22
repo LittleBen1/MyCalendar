7a5c142f2e026bd86ebafae288779343cc75ccf0
Benjamin <brgaultier@gmail.com>
2019-09-22 11:19:31 +0200

    Added registration and login

--- etnaCalendar/src/app/app.module.ts
+++ etnaCalendar/src/app/app.module.ts
@@ -8,11 +8,21 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import firebaseConfig from './firebase';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import config from './firebase';
+
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule,
+     IonicModule.forRoot(),
+     AppRoutingModule,
+     AngularFireModule.initializeApp(config),
+     AngularFireAuthModule
+    ],
   providers: [
     StatusBar,
     SplashScreen,
